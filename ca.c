b84e156127f0bfe6e6737a0f6048d9fd617c2cd4
Paul Fox <pgf@foxharp.boston.ma.us>
2011-05-23 10:00:00 -0400

    Reconstructed history: This is the second oldest version of the ca (now "rca") calculator I've been able to locate. The file date on this version was 2011-05-23.
    This version adds
        bitwise operators
        hex and octal input and display
        ability to push pi
        early integer and float math modes
        strips commas from its input

--- ca.c
+++ ca.c
@@ -15,7 +15,9 @@
 #include <string.h>
 #include <math.h>
 
-/* #define DEBUG */
+#define DEBUG 1
+#undef DEBUG
+
 #ifdef DEBUG
 #define debug(a) printf a 
 #else
@@ -30,7 +32,7 @@ typedef int opreturn;
 #define GOODOP 1
 #define BADOP 0
 
-double pi = 0.0;
+double pi;
 
 /* internal representation of numbers */
 struct num {
@@ -75,6 +77,12 @@ opreturn multiply(token *t);
 opreturn divide(token *t);
 opreturn y_to_the_x(token *t);
 opreturn modulus(token *t);
+opreturn rshift(token *t);
+opreturn lshift(token *t);
+opreturn and(token *t);
+opreturn or(token *t);
+opreturn xor(token *t);
+opreturn not(token *t);
 opreturn chsign(token *t);
 opreturn recip(token *t);
 opreturn squarert(token *t);
@@ -85,10 +93,19 @@ void printtop(void);
 void printstack(void);
 opreturn printall(token *t);
 opreturn printone(token *t);
+opreturn printhex(token *t);
+opreturn printoct(token *t);
+opreturn printdec(token *t);
+opreturn modehex(token *t);
+opreturn modeoct(token *t);
+opreturn modedec(token *t);
+opreturn modeinteger(token *t);
+opreturn modefloat(token *t);
 opreturn clear(token *t);
 opreturn rolldown(token *t);
 opreturn repush(token *t);
 opreturn exchange(token *t);
+opreturn push_pi(token *t);
 opreturn autop(token *t);
 opreturn quit(token *t);
 opreturn help(token *t);
@@ -100,6 +117,13 @@ struct num *stack;
 
 /* if true, print the top of stack after every user newline */
 boolean autoprint = TRUE;
+
+/* decimal, hex, or octal output */
+int pformat = 'd';
+int oformat = 'd';  /* previous value */
+
+/* integer or float math */
+int math = 'f';
 
 /* suppress "empty stack" messages */
 boolean silent = FALSE;
@@ -115,6 +139,13 @@ struct oper opers[] = {
 	{"*", multiply,		"multiply top two numbers" },
 	{"/", divide, 		"divide top two numbers" },
 	{"%", modulus, 		"take modulo of top two numbers" },
+	{">>", rshift, 		"right shift" },
+	{"<<", lshift, 		"left shift" },
+	{"&", and, 		"bitwise and" },
+	{"|", or, 		"bitwise or" },
+	{"^", xor, 		"bitwise xor" },
+	{"~", not, 		"bitwise not" },
+	{"", 0, 0},
 	{"changesign", chsign,	"negate top number" },
 	{"chs", chsign,		" \" \"" },
 	{"reciprocal", recip,	"take reciprocal of top number" },
@@ -125,8 +156,14 @@ struct oper opers[] = {
 	{"tan", tangent,	"take tangent of angle (in degrees)" },
 	{"^", y_to_the_x, 	"raise next-to-top to power of top" },
 	{"raise", y_to_the_x, 	" \" \"" },
-	{"print", printone, 	"print top of stack" },
+	{"", 0, 0},
 	{"Print", printall, 	"print whole stack" },
+	{"print", printone, 	"print top of stack" },
+	{"dprint", printdec, 	"print top of stack in decimal" },
+	{"oprint", printoct, 	"print top of stack in octal" },
+	{"hprint", printhex, 	"print top of stack in hex" },
+	{"xprint", printhex, 	" \" \"" },
+	{"", 0, 0},
 	{"clear", clear, 	"clear whole stack" },
 	{"pop", rolldown, 	"pop (and discard) top of stack" },
 	{"push", enter, 	"push (duplicate) top of stack" },
@@ -136,9 +173,19 @@ struct oper opers[] = {
 	{"repush", repush, 	" \" \"" },
 	{"xchange", exchange, 	"exchange top two numbers" },
 	{"exchange", exchange, 	" \" \"" },
-	{"autoprint", autop,	"toggle autoprinting" },
+	{"pi", push_pi, 	"push constant pi" },
+	{"", 0, 0},
+	{"Autoprint", autop,	"toggle autoprinting" },
+	{"Hex", modehex, 	"switch to hex output" },
+	{"X", modehex, 		" \" \"" },
+	{"Octal", modeoct, 	"switch to octal output" },
+	{"Decimal", modedec, 	"switch to decimal output" },
+	{"Integer", modeinteger,"switch to integer arithmetic" },
+	{"Float", modefloat, 	"switch to float arithmetic" },
+	{"", 0, 0},
 	{"quit", quit, 		"leave" },
 	{"exit", quit, 		" \" \"" },
+	{"", 0, 0},
 	{"help", help, 		NULL },
 	{"?", help, 		NULL },
 	{NULL, NULL}
@@ -217,6 +264,7 @@ gettoken(FILE *f)
 	double n;
 	static char *p = NULL;
 	static char *t = NULL;
+	static char *c = NULL;
 
 	if (p == NULL) {
 		if (fgets(inputline, 1024, f) == NULL)
@@ -243,9 +291,32 @@ gettoken(FILE *f)
 	if (*p) 
 		*p++ = '\0';
 
+	/* eliminate commas */
+	debug(("precomma %s\n", t));
+	while ((c = strchr(t, ',')) != NULL) {
+	    memmove(c, c+1, strlen(c));
+	}
+	debug(("postcomma %s\n", t));
+
 
 	/* is it a number? */
-	if (isdigit(*t) || (*t == '.') || (*t == '-' && isdigit(*(t+1)))) {
+	if (*t == '0' && (*(t+1) == 'x' || *(t+1) == 'X')) {
+		long ln = strtol(t, 0, 16);
+		tok.type = FLOAT;
+		tok.val.val = ln;
+		debug(("gettoken hex value is %g decimal\n", n));
+		return &tok;
+
+	} else if (*t == '0' && ('0' <= *(t+1) && *(t+1) <= '7')) {
+		long ln = strtol(t, 0, 8);
+		tok.type = FLOAT;
+		tok.val.val = ln;
+		debug(("gettoken octal value is %g decimal\n", n));
+		return &tok;
+
+	} else if (isdigit(*t) || (*t == '.') ||
+			(*t == '-' && isdigit(*(t+1)))) {
+	debug(("x postcomma %s\n", t));
 		if (sscanf(t,"%lg",&n) == 1) {
 			tok.type = FLOAT;
 			tok.val.val = n;
@@ -256,7 +327,7 @@ gettoken(FILE *f)
 		struct oper *op;
 		op = opers;
 		while (op->name) {
-			if (!strncmp(t, op->name, strlen(t))) {
+			if (*op->name && !strncmp(t, op->name, strlen(t))) {
 				tok.type = OP;
 				tok.val.opfunc = op->func;
 				debug(("gettoken op is %s\n", op->name));
@@ -278,7 +349,10 @@ push(double n)
 {
 	struct num *p = (struct num *)calloc(1, sizeof (struct num));
 	if (!p) errexit("no memory for push");
-	p->val = n;
+	if (math == 'i')
+	    p->val = (long)n;
+	else
+	    p->val = n;
 	p->next = stack;
 	stack = p;
 }
@@ -399,6 +473,108 @@ modulus( token *t )
 			return GOODOP;
 		}
 		push(b);
+	}
+	return BADOP;
+}
+
+opreturn
+rshift( token *t )
+{
+	double a, b;
+	if (pop(&b)) {
+		if (pop(&a)) {
+			int i, j;
+			i = (long)a;
+			j = (long)b;
+			push(i >> j);
+			lastx = b;
+			return GOODOP;
+		}
+		push(b);
+	}
+	return BADOP;
+}
+
+opreturn
+lshift( token *t )
+{
+	double a, b;
+	if (pop(&b)) {
+		if (pop(&a)) {
+			int i, j;
+			i = (long)a;
+			j = (long)b;
+			push(i << j);
+			lastx = b;
+			return GOODOP;
+		}
+		push(b);
+	}
+	return BADOP;
+}
+
+opreturn
+and( token *t )
+{
+	double a, b;
+	if (pop(&b)) {
+		if (pop(&a)) {
+			int i, j;
+			i = (long)a;
+			j = (long)b;
+			push(i & j);
+			lastx = b;
+			return GOODOP;
+		}
+		push(b);
+	}
+	return BADOP;
+}
+
+opreturn
+or( token *t )
+{
+	double a, b;
+	if (pop(&b)) {
+		if (pop(&a)) {
+			int i, j;
+			i = (long)a;
+			j = (long)b;
+			push(i | j);
+			lastx = b;
+			return GOODOP;
+		}
+		push(b);
+	}
+	return BADOP;
+}
+
+opreturn
+xor( token *t )
+{
+	double a, b;
+	if (pop(&b)) {
+		if (pop(&a)) {
+			int i, j;
+			i = (long)a;
+			j = (long)b;
+			push(i ^ j);
+			lastx = b;
+			return GOODOP;
+		}
+		push(b);
+	}
+	return BADOP;
+}
+
+opreturn
+not( token *t )
+{
+	double a;
+	if (pop(&a)) {
+		push( ~(long)a );
+		lastx = a;
+		return GOODOP;
 	}
 	return BADOP;
 }
@@ -526,10 +702,23 @@ void
 printtop (void)
 {
 	double n;
+	long ln;
 	if (pop(&n)) {
-		printf(" %g\n",n);
+		switch (pformat) {
+		case 'x':
+		    ln = n;
+		    printf(" 0x%lx\n",ln);
+		    break;
+		case 'o':
+		    ln = n;
+		    printf(" 0%lo\n",ln);
+		    break;
+		default:
+		    printf(" %.10g\n",n);
+		}
 		push(n);
 	}
+	pformat = oformat;
 }
 
 void
@@ -538,8 +727,8 @@ printstack (void)
 	double n;
 	if (pop(&n)) {
 		(void)printstack();
-		printf(" %g\n",n);
 		push(n);
+		printtop();
 	}
 }
 
@@ -568,6 +757,77 @@ printone ( token *t )
 		silent = FALSE;
 	}
 	return GOODOP;
+}
+
+opreturn
+printhex ( token *t )
+{
+	oformat = pformat;
+	pformat = 'x';
+	printone(t);
+	return GOODOP;
+}
+
+opreturn
+printoct ( token *t )
+{
+	oformat = pformat;
+	pformat = 'o';
+	printone(t);
+	return GOODOP;
+}
+
+opreturn
+printdec ( token *t )
+{
+	oformat = pformat;
+	pformat = 'd';
+	printone(t);
+	return GOODOP;
+}
+
+opreturn
+modehex ( token *t )
+{
+	oformat = pformat = 'x';
+	printf("output mode is now hex\n");
+	if (math != 'i')
+		printf("warning: floating math: values not truncated\n");
+	return printall(t);
+}
+
+opreturn
+modeoct ( token *t )
+{
+	oformat = pformat = 'o';
+	printf("output mode is now octal\n");
+	if (math != 'i')
+		printf("warning: floating math: values not truncated\n");
+	return printall(t);
+}
+
+opreturn
+modedec ( token *t )
+{
+	oformat = pformat = 'd';
+	printf("output mode is now decimal\n");
+	return printall(t);
+}
+
+opreturn
+modeinteger ( token *t )
+{
+	math = 'i';
+	printf("arithmetic mode is now integer\n");
+	return printall(t);
+}
+
+opreturn
+modefloat ( token *t )
+{
+	math = 'f';
+	printf("arithmetic mode is now integer\n");
+	return printall(t);
 }
 
 opreturn
@@ -611,6 +871,12 @@ exchange( token *t )
 }
 
 opreturn
+push_pi ( token *t )
+{
+	push(pi);
+}
+
+opreturn
 autop ( token *t )
 {
 	autoprint = !autoprint;
@@ -630,8 +896,11 @@ help ( token *t )
 	struct oper *op;
 	extern struct oper opers[];
 	op = opers;
-	while (op->name && op->help) {
-		printf("%-20s%s\n", op->name, op->help);
+	while (op->name) {
+		if (!*op->name)
+		    putchar('\n');
+		else
+		    printf("%-20s%s\n", op->name, op->help);
 		op++;
 	}
 	return GOODOP;
